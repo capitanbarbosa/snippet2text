aa1de03541389410569a6eaed455a8ebf6198365
capitanbarbosa <wizbarbosa@gmail.com>
2022-09-29 20:00:36 -0500

    Updated and re-patched vendored dependencies

--- snippet-text.py
+++ snippet-text.py
@@ -2,20 +2,20 @@ from PIL import ImageGrab, Image
 import PIL.Image
 import pytesseract
 import clipboard
-#--------- coge el clipboard y lo guarda (luego de que tomamos un snap)-------
+# --------- coge el clipboard y lo guarda (luego de que tomamos un snap)-------
 img = ImageGrab.grabclipboard()
 print(img)
 # print(isinstance(img, Image.Image))
 print(img.size)
 print(img.mode)
-img.save(r'C:\Users\luisd\OneDrive\Documents\Coding\Wiz Labs\Snippet-to-text\bruh.png')
-#--------- pasamos la imagen por tesseract--------
-#if no tesseract exe on our PATH, include:
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
+img.save('bruh.png')
+# --------- pasamos la imagen por tesseract--------
+# if no tesseract exe on our PATH, include:
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 
-bruh = pytesseract.image_to_string(PIL.Image.open(r'C:\Users\luisd\OneDrive\Documents\Coding\Wiz Labs\Snippet-to-text\bruh.png'))
+bruh = pytesseract.image_to_string(PIL.Image.open(
+    'bruh.png'))
 print(bruh)
 
-#------------------ clipboard windows + V -------------------
+# ------------------ clipboard windows + V -------------------
 clipboard.copy(bruh)
-
